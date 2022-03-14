cd55cdb304918d9d1ab829d82899cfb9431397e3
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2022-03-14 16:01:27 +0800

    update

---
+++ README.md
@@ -0,0 +1,82 @@
+# Implementing Searching for MobileNetV3 paper using Pytorch
+- The current model is a very early model. I will modify it as a general model as soon as possible.
+## Paper
+- [Searching for MobileNetV3 paper](https://arxiv.org/abs/1905.02244)
+- Author: Andrew Howard(Google Research), Mark Sandler(Google Research, Grace Chu(Google Research), Liang-Chieh Chen(Google Research), Bo Chen(Google Research), Mingxing Tan(Google Brain), Weijun Wang(Google Research), Yukun Zhu(Google Research), Ruoming Pang(Google Brain), Vijay Vasudevan(Google Brain), Quoc V. Le(Google Brain), Hartwig Adam(Google Research)
+
+## Todo
+- Experimental need for ImageNet dataset.
+- Code refactoring
+
+## MobileNetV3 Block
+![캡처](https://user-images.githubusercontent.com/22078438/57360577-6f30d000-71b5-11e9-89a6-24034a3ecdde.PNG)
+
+## Experiments
+- For CIFAR-100 data, I experimented with resize (224, 224).<br>
+
+| Datasets | Model | acc1 | acc5 | Epoch  | Parameters
+| :---: | :---: | :---: | :---: | :---: | :---: |
+CIFAR-100 | MobileNetV3(LARGE) | 70.44% | 91.34% | 80 | 3.99M
+CIFAR-100 | MobileNetV3(SMALL) | 67.04% | 89.41% | 55 | 1.7M
+IMAGENET | MobileNetV3(LARGE) WORK IN PROCESS | | | | 5.15M
+IMAGENET | MobileNetV3(SMALL) WORK IN PROCESS | | | | 2.94M
+
+## Usage
+
+### Train
+```
+python main.py
+```
+- If you want to change hyper-parameters, you can check "python main.py --help"
+
+Options:
+- `--dataset-mode` (str) - which dataset you use, (example: CIFAR10, CIFAR100), (default: CIFAR100).
+- `--epochs` (int) - number of epochs, (default: 100).
+- `--batch-size` (int) - batch size, (default: 128).
+- `--learning-rate` (float) - learning rate, (default: 1e-1).
+- `--dropout` (float) - dropout rate, (default: 0.3).
+- `--model-mode` (str) - which network you use, (example: LARGE, SMALL), (default: LARGE).
+- `--load-pretrained` (bool) - (default: False).
+- `--evaluate` (bool) - Used when testing. (default: False).
+- `--multiplier` (float) - (default: 1.0).
+
+### Test
+```
+python main.py --evaluate True
+```
+- Put the saved model file in the checkpoint folder and saved graph file in the saved_graph folder and type "python main.py --evaluate True".
+- If you want to change hyper-parameters, you can check "python test.py --help"
+
+Options:
+- `--dataset-mode` (str) - which dataset you use, (example: CIFAR10, CIFAR100), (default: CIFAR100).
+- `--epochs` (int) - number of epochs, (default: 100).
+- `--batch-size` (int) - batch size, (default: 128).
+- `--learning-rate` (float) - learning rate, (default: 1e-1).
+- `--dropout` (float) - dropout rate, (default: 0.3).
+- `--model-mode` (str) - which network you use, (example: LARGE, SMALL), (default: LARGE).
+- `--load-pretrained` (bool) - (default: False).
+- `--evaluate` (bool) - Used when testing. (default: False).
+- `--multiplier` (float) - (default: 1.0).
+
+### Number of Parameters
+```python
+import torch
+
+from model import MobileNetV3
+
+def get_model_parameters(model):
+    total_parameters = 0
+    for layer in list(model.parameters()):
+        layer_parameter = 1
+        for l in list(layer.size()):
+            layer_parameter *= l
+        total_parameters += layer_parameter
+    return total_parameters
+
+tmp = torch.randn((128, 3, 224, 224))
+model = MobileNetV3(model_mode="LARGE", multiplier=1.0)
+print("Number of model parameters: ", get_model_parameters(model))
+```
+
+## Requirements
+- torch==1.0.1
---
+++ main.py
@@ -0,0 +1,272 @@
+import argparse
+import os
+import time
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from model import MobileNetV3
+from preprocess import load_data
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def get_args():
+    parser = argparse.ArgumentParser("parameters")
+
+    parser.add_argument("--dataset-mode", type=str, default="IMAGENET", help="(example: CIFAR10, CIFAR100, IMAGENET), (default: IMAGENET)")
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs, (default: 100)")
+    parser.add_argument("--batch-size", type=int, default=512, help="number of batch size, (default, 512)")
+    parser.add_argument("--learning-rate", type=float, default=1e-1, help="learning_rate, (default: 1e-1)")
+    parser.add_argument("--dropout", type=float, default=0.8, help="dropout rate, not implemented yet, (default: 0.8)")
+    parser.add_argument('--model-mode', type=str, default="LARGE", help="(example: LARGE, SMALL), (default: LARGE)")
+    parser.add_argument("--load-pretrained", type=bool, default=False, help="(default: False)")
+    parser.add_argument('--evaluate', type=bool, default=False, help="Testing time: True, (default: False)")
+    parser.add_argument('--multiplier', type=float, default=1.0, help="(default: 1.0)")
+    parser.add_argument('--print-interval', type=int, default=5, help="training information and evaluation information output frequency, (default: 5)")
+    parser.add_argument('--data', default='D:/ILSVRC/Data/CLS-LOC')
+    parser.add_argument('--workers', type=int, default=4)
+    parser.add_argument('--distributed', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+# reference,
+# https://github.com/pytorch/examples/blob/master/imagenet/main.py
+# Thank you.
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(train_loader), batch_time, data_time, losses, top1, top5, prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (data, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        data, target = data.to(device), target.to(device)
+
+        # if args.gpu is not None:
+        #     data = data.cuda(args.gpu, non_blocking=True)
+        # target = target.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output = model(data)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), data.size(0))
+        top1.update(acc1[0], data.size(0))
+        top5.update(acc5[0], data.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_interval == 0:
+            progress.print(i)
+
+
+def validate(val_loader, model, criterion, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
+                             prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (data, target) in enumerate(val_loader):
+            # if args.gpu is not None:
+            #     input = input.cuda(args.gpu, non_blocking=True)
+            # target = target.cuda(args.gpu, non_blocking=True)
+            data, target = data.to(device), target.to(device)
+
+            # compute output
+            output = model(data)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), data.size(0))
+            top1.update(acc1[0], data.size(0))
+            top5.update(acc5[0], data.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_interval == 0:
+                progress.print(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg, top5.avg
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, *meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def print(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def main():
+    args = get_args()
+    train_loader, test_loader = load_data(args)
+
+    if args.dataset_mode == "CIFAR10":
+        num_classes = 10
+    elif args.dataset_mode == "CIFAR100":
+        num_classes = 100
+    elif args.dataset_mode == "IMAGENET":
+        num_classes = 1000
+    print('num_classes: ', num_classes)
+
+    model = MobileNetV3(model_mode=args.model_mode, num_classes=num_classes, multiplier=args.multiplier, dropout_rate=args.dropout).to(device)
+    if torch.cuda.device_count() >= 1:
+        print("num GPUs: ", torch.cuda.device_count())
+        model = nn.DataParallel(model).to(device)
+
+    if args.load_pretrained or args.evaluate:
+        filename = "best_model_" + str(args.model_mode)
+        checkpoint = torch.load('./checkpoint/' + filename + '_ckpt.t7')
+        model.load_state_dict(checkpoint['model'])
+        epoch = checkpoint['epoch']
+        acc1 = checkpoint['best_acc1']
+        acc5 = checkpoint['best_acc5']
+        best_acc1 = acc1
+        print("Load Model Accuracy1: ", acc1, " acc5: ", acc5, "Load Model end epoch: ", epoch)
+    else:
+        print("init model load ...")
+        epoch = 1
+        best_acc1 = 0
+
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=1e-5, momentum=0.9)
+    # optimizer = optim.RMSprop(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    if args.evaluate:
+        acc1, acc5 = validate(test_loader, model, criterion, args)
+        print("Acc1: ", acc1, "Acc5: ", acc5)
+        return
+
+    if not os.path.isdir("reporting"):
+        os.mkdir("reporting")
+
+    start_time = time.time()
+    with open("./reporting/" + "best_model_" + args.model_mode + ".txt", "w") as f:
+        for epoch in range(epoch, args.epochs):
+            adjust_learning_rate(optimizer, epoch, args)
+            train(train_loader, model, criterion, optimizer, epoch, args)
+            acc1, acc5 = validate(test_loader, model, criterion, args)
+
+            is_best = acc1 > best_acc1
+            best_acc1 = max(acc1, best_acc1)
+
+            if is_best:
+                print('Saving..')
+                best_acc5 = acc5
+                state = {
+                    'model': model.state_dict(),
+                    'best_acc1': best_acc1,
+                    'best_acc5': best_acc5,
+                    'epoch': epoch,
+                }
+                if not os.path.isdir('checkpoint'):
+                    os.mkdir('checkpoint')
+                filename = "best_model_" + str(args.model_mode)
+                torch.save(state, './checkpoint/' + filename + '_ckpt.t7')
+
+            time_interval = time.time() - start_time
+            time_split = time.gmtime(time_interval)
+            print("Training time: ", time_interval, "Hour: ", time_split.tm_hour, "Minute: ", time_split.tm_min, "Second: ", time_split.tm_sec, end='')
+            print(" Test best acc1:", best_acc1, " acc1: ", acc1, " acc5: ", acc5)
+
+            f.write("Epoch: " + str(epoch) + " " + " Best acc: " + str(best_acc1) + " Test acc: " + str(acc1) + "\n")
+            f.write("Training time: " + str(time_interval) + " Hour: " + str(time_split.tm_hour) + " Minute: " + str(
+                time_split.tm_min) + " Second: " + str(time_split.tm_sec))
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
